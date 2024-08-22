cb6989ba62e9494115f0d370732a7c8bbc59402b
victor_moreira <victormoreira@vivaldi.net>
2024-08-22 11:15:16 -0300

    Erro compreendido

--- src/test/java/com/aranduteck/kataLogoBooks/controller/BookControllerTest.java
+++ src/test/java/com/aranduteck/kataLogoBooks/controller/BookControllerTest.java
@@ -5,10 +5,12 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.aranduteck.kataLogoBooks.model.Book;
 import com.aranduteck.kataLogoBooks.model.Reader;
+import com.aranduteck.kataLogoBooks.model.dto.BookDto;
 import com.aranduteck.kataLogoBooks.service.BookService;
 import com.aranduteck.kataLogoBooks.view.BookView;
 
@@ -18,26 +20,32 @@ public class BookControllerTest {
     private BookController controller;
     private BookService service;
     private BookView view;
+    private BookDto dto;
+    private Reader reader;
 
     private String author = "Machado";
     private String title = "Capitu";
 
+    @BeforeEach
     public void setUp(){
         
-        model = new Book(title, author);
-        service = new BookService(model);
+        // view = new BookView(dto);
+        view = mock(BookView.class);
+        // model = new Book(title, author);
+        model = mock(Book.class);
+        // service = new BookService(model);
+        service = mock(BookService.class);
         controller = new BookController(service, view);
-      
-        // service = mock(BookService.class);
-        // view = mock(BookView.class);
-               
+ 
     }
 
     @Test
     public void getBorowTest(){
 
-        Reader reader = mock(Reader.class);
-// TODO: por que tá dando erro?
+        
+// 
         assertNull( controller.getBorowTo());
+
     }
+
 }
